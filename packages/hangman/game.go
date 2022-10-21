dd50ee3e45cbfa2e95c907ede05a30bceff508dd
evzs <evachbn@gmail.com>
2022-10-21 17:34:58 +0200

    ui

--- packages/hangman/game.go
+++ packages/hangman/game.go
@@ -11,9 +11,10 @@ import (
 )
 
 type HangManData struct {
-	Word      string // Word composed of '_', ex: H_ll_
-	FinalWord string // Final word chosen by the program at the beginning. It is the word to find
-	Attempts  int    // Number of attempts left
+	Word        string // Word composed of '_', ex: H_ll_
+	FinalWord   string // Final word chosen by the program at the beginning. It is the word to find
+	Attempts    int    // Number of attempts left
+	UsedLetters []string
 	//HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
 }
 
