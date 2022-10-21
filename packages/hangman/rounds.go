382ecf686ca3545d82b24feb625e60e4484b99e3
evzs <evachbn@gmail.com>
2022-10-21 18:58:13 +0200

    stocking letters

--- packages/hangman/rounds.go
+++ packages/hangman/rounds.go
@@ -32,7 +32,7 @@ func (data *HangManData) PrintWord() {
 func NewRound(data *HangManData) {
 	// Asks user for input and processes the answer.
 	answer := utils.GetUserInput()
-	processed := ProcessAnswer(data.FinalWord, answer)
+	processed := data.ProcessAnswer(data.FinalWord, answer)
 
 	if processed == 1 {
 		// Word has been discovered
@@ -60,18 +60,35 @@ func (data *HangManData) RevealLetter(answer string) {
 	}
 }
 
+func (data *HangManData) AddUsedLetters(letter string) {
+	for _, char := range data.UsedLetters {
+		if char == letter {
+			return
+		}
+
+	}
+	data.UsedLetters = append(data.UsedLetters, letter)
+}
+
+func (data *HangManData) PrintStockedLetters() {
+	fmt.Println(data.UsedLetters)
+}
+
 // Returns an int based on the points the player should lose or not.
 // If 0, a letter had been discovered, if > 0, points should be lost.
 // If 1, the right word has been suggested, the player has won.
-func ProcessAnswer(word, answer string) int {
+func (data *HangManData) ProcessAnswer(word, answer string) int {
 	// Answer = 1 character
 	if len(answer) == 1 {
+		data.AddUsedLetters(answer)
+		data.PrintStockedLetters()
 		if strings.Contains(word, string(answer[0])) {
 			return 0
 		} else {
 			return -1
 		}
 		// Answer = at least 2 characters.
+
 	} else {
 		if answer == word {
 			return 1
